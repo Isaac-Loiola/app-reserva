03eb144bc98e1833c6c1627f4902599df90ce74a
Isaac Oliveira Loiol <isaackopi@icloud.com>
2025-11-27 10:52:25 -0300

    adicionando ion-item para exibir as reservas

--- src/app/reservas-list/reservas-list.page.ts
+++ src/app/reservas-list/reservas-list.page.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar, AlertController } from '@ionic/angular/standalone';
+import { IonContent, IonHeader, IonTitle, IonToolbar, AlertController, IonButtons, IonMenuButton, IonItem, IonText, IonLabel } from '@ionic/angular/standalone';
 import { Router } from '@angular/router';
 
 @Component({
@@ -9,7 +9,7 @@ import { Router } from '@angular/router';
   templateUrl: './reservas-list.page.html',
   styleUrls: ['./reservas-list.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
+  imports: [IonLabel, IonItem, IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule, IonButtons, IonMenuButton, IonText]
 })
 export class ReservasListPage implements OnInit {
 
